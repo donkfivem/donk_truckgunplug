653ac405e896d57fdafed667825d749dbb037471
donk <unicomp22@gmail.com>
2025-08-12 03:46:12 +0100

    added sql

---
+++ install/sql.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `gunplug` (
+  `identifier` varchar(50) DEFAULT NULL,
+  `time` int(11) DEFAULT NULL,
+  `item_count` int(11) DEFAULT 30
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
